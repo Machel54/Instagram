0c8e337c1acba6f40a94a039b2a2dba6812871e5
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-11-18 04:44:14 +0300

    Create Model tags and initialize it to the db

--- instagram/models.py
+++ instagram/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class User(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    
+    def __str__(self):
+        return self.first_name
+    
+class Profile(models.Model):
+    
+    
+class tags(models.Model):
+    name = models.CharField(max_length= 20)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
