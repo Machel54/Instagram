655a6481963bf40209e318adc81e04823923e1bc
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-11-19 04:55:16 +0300

    Update Profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
+# class Editor(models.Model):
+#     first_name = models.CharField(max_length =30)
+#     last_name = models.CharField(max_length =30)
+#     email = models.EmailField()
     
-    def __str__(self):
-        return self.first_name
-    class Meta:
-        ordering = ['first_name']
-    def save_editor(self):
-        self.save()
+#     def __str__(self):
+#         return self.first_name
+#     class Meta:
+#         ordering = ['first_name']
+#     def save_editor(self):
+#         self.save()
     
 class tags(models.Model):
     name = models.CharField(max_length= 20)
@@ -25,12 +26,51 @@ class tags(models.Model):
         verbose_name_plural = 'tags'
         
 class Profile(models.Model):
+    editor = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_image = models.ImageField( upload_to='profile_pics')
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
-    editor = models.ForeignKey(Editor)
-    profile_image = models.ImageField(upload_to = 'profile')
-    bio = models.TextField()
+    bio = models.TextField(default="Type your bio")
+    
+    def __str__(self):
+        return f'{self.user.username}'
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+        
+    def delete_profile(self):
+        self.delete()
+        
+    def comment(self, photo, text):
+        Comment(text=text, photo=photo, user=self).save()
+        
+    @classmethod
+    def search_by_user(cls,search_term):
+        profiles = cls.objects.filter(user__name__icontains=search_term)
+        return profiles
+    
+    def post(self, form):
+        image = form.save(commit=False)
+        image.user = self
+        image.save()
+        
+        
+    class Meta:
+        ordering = ['user']
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+        
+    @property
+    def all_comments(self):
+        return self.comments.all()
+
     
 class NewsletterRecipients(models.Model):
     name = models.CharField(max_length=30)
