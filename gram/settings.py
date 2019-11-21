78974407a1f28299c1d93a09e69cbf24278dc3ca
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-11-21 14:42:20 +0300

    deployment to heroku

--- gram/settings.py
+++ gram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,10 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Activate django heroku.
+django_heroku.settings(locals())
--- instagram/tests.py
+++ instagram/tests.py
@@ -1,17 +1,46 @@
 from django.test import TestCase
 from .models import User,Profile,Comment
 
-# Create your tests here.
-class UserTestClass(TestCase):
+# # Create your tests here.
+# class UserTestClass(TestCase):
     
-  # Set up method
+#   # Set up method
+#     def setUp(self):
+#         self.logue= User(first_name='Flacko', last_name ='Hearts', email ='logue54@gmail.com')
+#     # Testing  instance
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.logue,User))
+#        # Testing Save Method
+#     def test_save_method(self):
+#         self.logue.save_user()
+#         users = User.objects.all()
+#         self.assertTrue(len(users) > 0)
+        
+class ProfileTestClass(TestCase):
+    
+    '''
+    This is a class that perform unnittest  behaviour on the Profile Model.
+    '''
+    
     def setUp(self):
-        self.logue= User(User_name = 'Logue54',first_name='Flacko', last_name ='Hearts', email ='logue54@gmail.com')
-    # Testing  instance
+        self.profile_two = Profile(profile_image='images/mine.jpg',bio='i love anime',user_id=3)
+        
+        
     def test_instance(self):
-        self.assertTrue(isinstance(self.logue,User))
-       # Testing Save Method
+        self.assertTrue(isinstance(self.profile_two,Profile)) 
+
     def test_save_method(self):
-        self.logue.save_user()
-        users = User.objects.all()
-        self.assertTrue(len(users) > 0)
+        
+        self.profile_two.save_user_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile_two.save_user_profile()
+        self.profile_two.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) is 0)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+    
